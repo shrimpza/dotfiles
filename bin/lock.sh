e074f7c38961c149b5da7d7c3ca7ca87464247e9
Kenneth Watson <git@shrimpworks.za.net>
2021-12-09 11:57:47 +0800

    updates for laptop with external display, rofi 1.7 update

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,7 +2,7 @@
  
 #ICON=$HOME/Pictures/lock.png
 IMAGE=/tmp/screen.png
-scrot /tmp/screen.png
+scrot -o /tmp/screen.png
 convert $IMAGE -scale 5% -scale 2000% $IMAGE #-fill black -colorize 25% $IMAGE
 #convert $TMPBG -type Grayscale -blur 0x04 $TMPBG
 #convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
